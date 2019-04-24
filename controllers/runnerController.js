3c0a18d85746352241d67d37edd70e2b58f0bbfb
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-24 15:04:03 -0600

    got server to run again

--- controllers/runnerController.js
+++ controllers/runnerController.js
@@ -1,8 +1,7 @@
 const express = require('express');
-
 const router  = express.Router();
 const runner  = require('../models/runner');
-const event   = require('../models/event');
+
 
 router.get ('/', async (req, res)=>{
     try {
