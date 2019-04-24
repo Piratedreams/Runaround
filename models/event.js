2eda121acdfe5f8b2348482dd166a652e173d5e6
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-24 10:30:36 -0600

    fixed some errors

--- models/event.js
+++ models/event.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
 const eventSchema = mongoose.Schema({
-  time: {type:Numbers},
-  location: {type:Location}
-})
+  eventName: {type: String, required: true},
+  time: {type: Number, required: true},
+  location: {type: String, required: true},
+  entranceFee: {type: Number, required: true},
+  RSVP: {type: Boolean, required: true}
+});
 
 const Event = mongoose.model('Event', eventSchema)
 
-module.exports = Event
+module.exports = Event;
