3253c67cc161ce8476c0f98dd0ab76035a03af6f
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-25 12:05:32 -0600

    still fixing routes for events but progress

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/event');
+const mongoose = require('mongoose');
 
 
-router.get('/', (req, res) => {
-    res.render('event/index.ejs')
-});
-
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const foundEvent = await Event.find({})
-        res.render('event/show.ejs', {
-            events: foundEvent
-        });
-    } catch (err){
-        res.send(err);
-    }
-});
-
-router.get('/new', async (req, res) => {
-    try {
-        const foundEvent = await Event.find({})
-        res.render('event/new.ejs')
+        const foundEvent = await Event.find({});
+    res.render('event/index.ejs', {
+        events: foundEvent
+    });
     } catch (err) {
         res.send(err)
     }
 });
+
+
+router.get('/event/new', (req, res) => {
+    res.render('event/new.ejs')
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const allEvent = await Event.findById(req.params.Id)
+        res.render('event/edit.ejs', {
+            event: allEvent
+        });
+    } catch (err){
+        console.log(err);
+        res.send(err);
+    }
+});
+
 
 router.post('/event', async (req, res) => {
     try {
@@ -40,7 +46,7 @@ router.post('/event', async (req, res) => {
 
 router.get('/:id/edit', async (req, res) => {
     try {
-        const foundEvent = await Event.findById(req.params.id)
+        const foundEvent = await Event.findById(req.params.Id)
         res.render('event/edit.ejs', {
             event: foundEvent
         })
@@ -51,7 +57,7 @@ router.get('/:id/edit', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try{
-        const updateEvent = await Event.findByIdAndUpdate(req.params.id, {new: true});
+        const updateEvent = await Event.findByIdAndUpdate(req.params.id);
         res.redirect('/event')
     } catch (err) {
         res.send(err)
@@ -63,7 +69,9 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try{
-        const deletedEvent = await Event.delete(req.params.id);
+        const foundEvent = await Event.findByIdAndDelete(req.params.id);
+        const deleteEvent = await Event.findOne({'event': req.params.id})
+        
         res.redirect('/event')
     } catch (err) {
         res.send(err);
