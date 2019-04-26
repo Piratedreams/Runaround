2956a100fb9d2ef758a00c316d372e8ca5a5b9c7
Tommie <tommiealcorn@gmail.com>
2019-04-25 18:40:46 -0600

    cleanup & edit

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Event = require('../models/event');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
 
 router.get('/', async (req, res) => {
--- controllers/runnerController.js
+++ controllers/runnerController.js
@@ -6,66 +6,39 @@ const runner  = require('../models/runner');
 router.get ('/', async (req, res)=>{
     try {
         const foundRunner = await runner.find({});
+       
         res.render('runner/index.ejs', {
-        })
+            runner: foundRunner
+        });
     } catch(err){
         res.send(err)
     }
-
-    // runners.find({}, (err, runnersFromTheDatabase)=>{
-    //     res.render('runner/index.ejs', {
-    //         runnersOnTheTemplate: runnersFromTheDatabase
-    //     });
-    // });
 });
 
 
-// router.get('/new', (req, res)=>{
-//     res.render('runner/new.ejs');
-// })
-
 router.get('/:id', async (req, res)=>{
-
     try {
         const foundRunner = await runner.findById(req,params,id);
-        res.render('show.ejs', {
+        res.render('runner/show.ejs', {
           runner: foundRunner
         });
 
         } catch(err){
             res.send(err)
         }
-    // runner.
-    // findById(req.params.id)  
-    // .populate('runners')
-    // .exec((err, runnerFromTheDatabase)=>{
-    //     if(err){
-    //         res.send(err);
-    //     } else {
-    //         res.render('runner/show.ejs', {
-    //             runnerOnTheTemplate: runnerFromTheDatabase});
-    //     }
-    // })
 });
 
 router.get('/:id/edit', async (req, res)=>{
-    const foundRunner = await runnerFindById(req.params.id)
-
     try {
-        res.render('new.ejs', {
+        const foundRunner = await runnerFindById(req.params.id)
+        res.render('runner/new.ejs', {
             runner: foundRunner
         });
 
         } catch(err){
             res.send(err)
         }
-    
-    // Runner.findById(req.params.id, (err, runnerFromTheDatabase)=>{
-    //     res.render('runner/edit.ejs', {
-    //         runnerOnTheTemplate: runnerFromTheDatabase
-    //     })
-    // })
-})
+});
 
 router.post('/', async (req, res)=>{
     try {
@@ -75,48 +48,28 @@ router.post('/', async (req, res)=>{
     } catch(err){
         res.send(err)
     }
-    
-    // Runner.create(req.body, (err, newlyCreatedRunner)=>{
-    //     console.log(newlyCreatedRunner)
-    //     res.redirect('/runner')
-    // })
 });
 
 router.put('/:id', async (req, res)=>{
     try {
         const updateRunner = await runner.findByIdAndUpdate(req.params.id, req.body, {new: true});
-        res.redirect('/runners');
+        res.redirect('/runner');
 
     } catch(err){
         res.send(err);
     }
-    // Runner.findByIdAndUpdate(req.params.id, req.body, (err, runnerFromTheDatabase)=>{
-    //     console.log(runnerFromTheDatabase)
-    //     res.redirect('/runner');
-    // })
-})
+});
 
 router.delete('/:id', async (req, res)=>{
 
     try {
         const deleteRunner = await runner.delete(req.body)
-        res.redirect('/runners')
+        res.redirect('/runner')
 
         } catch(err){
             res.show(err)
         }
     });
-    // runner.findByIdAndDelete(req.params.id, (err, runnerFromTheDatabase)=>{
-    //     console.log(runnerFromTheDatabase);
-    //     runner.deleteMany({
-    //         _id: {
-    //             $in: runnerFromTheDatabase.runner
-    //         }
-    //     }, (err, data)=>{
-    //         console.log(data);
-    //         res.redirect('/runner');
-    //     })
-    // })
 
 
 module.exports = router;
